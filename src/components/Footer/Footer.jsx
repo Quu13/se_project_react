39fecb7868dcbf8ba8a3bf8f02347b4da8669d5a
Benicen <Benicensoto@Gmail.com>
2024-12-17 22:23:42 -0500

    Big changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="footer__content">
+      <div>
+        <p className="footer__developer-text">Developed By Benicen Soto</p>
+      </div>
+      <div>
+        <p className="footer__year-text">{currentYear}</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,30 @@
+import "./Main.css";
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+
+function Main({ weatherData, handleCardClick }) {
+  return (
+    <main>
+        <WeatherCard weatherData={weatherData} />
+        <section className="main__cards">
+            <p className="main__cards-text">Today is {weatherData.temp.F} &deg; F / You may want to wear:
+         </p>
+         <ul className="main__cards-list">
+          {defaultClothingItems
+            //.filter((item)  => {
+            //return item.weather === weatherData.type;
+          //})
+          .map((item) => {
+            return <ItemCard  
+            key={item._id} 
+            item={item} 
+            onCardClick={handleCardClick} />;
+        })}
+        </ul>
+     </section>
+  </main> 
+  )  
+}
+
+export default Main;
